f13d44dab4b8b95b14ff5fb1ebc01f6367000078
Chojan Shang <psiace@outlook.com>
2023-03-25 21:57:16 +0800

    refactor: split coauthor and team, fix bug
    Signed-off-by: Chojan Shang <psiace@outlook.com>

--- src/summarize.py
+++ src/summarize.py
@@ -5,55 +5,42 @@ import codecs
 
 from collections import defaultdict
 
-from values import SURVEY_PATH, AUTHOR_RELATIONSHIPS_PATH, AUTHOR_RELATIONSHIPS_WITH_METRICS_PATH, AUTHOR_RELATIONSHIPS_RESULTS_PATH
+from values import SURVEY_PATH, RELATIONSHIPS_PATH, RELATIONSHIPS_WITH_METRICS_PATH, COAUTHOR_RELATIONSHIPS_RESULTS_PATH, TEAM_RELATIONSHIPS_RESULTS_PATH
 
 with codecs.open(SURVEY_PATH, mode='r', encoding='utf8', errors='ignore') as f:
     papers = ujson.load(f)
     transactions = [paper["authors"] for paper in papers]
 
-# Train the FP-Growth model
-min_support = 5  # Set a minimum support threshold
+min_support = 5
 patterns = pyfpgrowth.find_frequent_patterns(transactions, min_support)
 
-# Generate association rules
-min_confidence = 0.5  # Set a minimum confidence threshold
+min_confidence = 0.5
 rules = pyfpgrowth.generate_association_rules(patterns, min_confidence)
 
-# Extract co-author and team relationships
-author_relationships = defaultdict(list)
+relationships = defaultdict(list)
 
 for rule in rules.items():
     antecedent, (consequent, confidence) = rule
     if len(antecedent) == 1 and len(consequent) == 1:
-        author_relationships["coauthors"].append({
+        relationships["coauthors"].append({
             "author1": antecedent[0],
             "author2": consequent[0],
             "confidence": confidence
         })
     elif len(antecedent) + len(consequent) > 2:
-        author_relationships["teams"].append({
+        relationships["teams"].append({
             "team_members": list(antecedent) + list(consequent),
             "confidence": confidence
         })
 
-# Calculate the total number of publications for each author
-author_publications = defaultdict(int)
+publications = defaultdict(int)
 
 for paper in papers:
     for author in paper["authors"]:
-        author_publications[author] += 1
+        publications[author] += 1
 
-# Calculate activity scores for co-author relationships
-for coauthor in author_relationships["coauthors"]:
-    num_relationship = patterns.get((coauthor["author1"], coauthor["author2"]), 0)
-    num_author1 = author_publications[coauthor["author1"]]
-    num_author2 = author_publications[coauthor["author2"]]
-    activity_score = num_relationship / (num_author1 + num_author2 - num_relationship)
-    coauthor["activity_score"] = activity_score
-
-# Calculate activity scores for team relationships
-with open(AUTHOR_RELATIONSHIPS_PATH, "w") as f:
-    ujson.dump(author_relationships, f, indent=4)
+with open(RELATIONSHIPS_PATH, "w") as f:
+    ujson.dump(relationships, f, indent=4)
 
 def calculate_lift(num_antecedent, num_consequent, num_relationship, num):
     return (num_relationship * num) / (num_antecedent * num_consequent)
@@ -84,28 +71,35 @@ def calculate_all_metrics(item, patterns, antecedent, consequent):
     item["max_confidence"] = float(0) if check_nonzero else calculate_max_confidence(num_antecedent, num_consequent, num_relationship)
     item["kulc"] = float(0) if check_nonzero else calculate_kulc(num_antecedent, num_consequent, num_relationship)
 
-for coauthor in author_relationships["coauthors"]:
+for coauthor in relationships["coauthors"]:
+    num_relationship = patterns.get((coauthor["author1"], coauthor["author2"]), 0)
+    num_author1 = publications[coauthor["author1"]]
+    num_author2 = publications[coauthor["author2"]]
     antecedent = (coauthor["author1"],)
     consequent = (coauthor["author2"],)
-
+    activity_score = num_relationship / (num_author1 + num_author2 - num_relationship)
+    coauthor["activity_score"] = activity_score
     calculate_all_metrics(coauthor, patterns, antecedent, consequent)
 
-for team in author_relationships["teams"]:
+for team in relationships["teams"]:
     antecedent = tuple(sorted(team["team_members"][:-1]))
     consequent = (team["team_members"][-1],)
 
     calculate_all_metrics(team, patterns, antecedent, consequent)
 
-with open(AUTHOR_RELATIONSHIPS_WITH_METRICS_PATH, "w") as f:
-    ujson.dump(author_relationships, f, indent=4)
+with open(RELATIONSHIPS_WITH_METRICS_PATH, "w") as f:
+    ujson.dump(relationships, f, indent=4)
 
-with open(AUTHOR_RELATIONSHIPS_RESULTS_PATH, "w", newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(["Type", "Author1", "Author2", "Support", "Lift", "Cosine", "Full Confidence", "Max Confidence", "Kulc"])
+with open(COAUTHOR_RELATIONSHIPS_RESULTS_PATH, "w", newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(["Author1", "Author2", "Confidence", "Activity Score", "Support", "Lift", "Cosine", "Full Confidence", "Max Confidence", "Kulc"])
 
-    for coauthor in author_relationships["coauthors"]:
-        writer.writerow(["Coauthor"] + list(coauthor.values()))
+    for coauthor in relationships["coauthors"]:
+        writer.writerow(list(coauthor.values()))
 
-    for team in author_relationships["teams"]:
-        team_members = ', '.join(team["team_members"])
-        writer.writerow(["Team"] + [team_members] + list(team.values())[1:])
+with open(TEAM_RELATIONSHIPS_RESULTS_PATH, "w", newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(["Members", "Confidence", "Support", "Lift", "Cosine", "Full Confidence", "Max Confidence", "Kulc"])
+    for team in relationships["teams"]:
+        team_members = ', '.join(map(str, team["team_members"]))
+        writer.writerow([team_members] + list(team.values())[1:])
